fdf461749b330e48fe5bb55db30a151905f604ca
MaFlet <mfletcher2808@hotmail.com>
2024-06-18 08:25:14 +0000

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This is the preferred betty style
+ * comments in C source code
+ * /
+ 
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return 0;
+}
