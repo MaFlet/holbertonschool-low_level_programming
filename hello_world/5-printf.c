62e5679d1ebd04d60bb5e45ddbdb9472cd76cb51
MaFlet <mfletcher2808@hotmail.com>
2024-06-18 10:15:01 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ *main - Prints entry
  *Return: 0 (Success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
