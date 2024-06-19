bf70039a9975cc0634c67cb4e0cf0a6463c50d87
MaFlet <mfletcher2808@hotmail.com>
2024-06-19 05:22:32 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,15 +6,22 @@
 */
 int main(void)
 {
-int num;
+int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Input a number: ");
-scanf("%d", &num);
 
-if (num >= 0)
-printf("%d is a positive number \n", num);
+if (n > 0)
+{
+printf("%d is a positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero \n", n);
+}
 else
-printf("%d is a negative number \n", num);
+{
+printf("%d is negative\n", n);
+}
 return (0);
 }
