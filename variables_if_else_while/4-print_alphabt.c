ea110d630442c04880f6396b02599396c769eeb7
MaFlet <mfletcher2808@hotmail.com>
2024-06-19 12:57:11 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase
+*Return: 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z' ; letter++)
+putchar(letter);
+if (letter == 'q', letter == 'e')
+putchar('\n');
+return (0);
+}
