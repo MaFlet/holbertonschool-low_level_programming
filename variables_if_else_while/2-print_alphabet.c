ce4a0e628294eb91101739a6797c165bae27af74
MaFlet <mfletcher2808@hotmail.com>
2024-06-19 12:08:59 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,8 +5,9 @@
 */
 int main(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+char letter;
+for (letter = 'a'; letter <= 'z' ; letter++)
+putchar(letter);
 putchar('\n');
 return (0);
 }
