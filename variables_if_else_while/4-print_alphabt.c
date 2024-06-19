89c3824cfb22dafa8b3b6fc427772b7babaf48f4
MaFlet <mfletcher2808@hotmail.com>
2024-06-19 13:04:52 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@ int main(void)
 char letter;
 for (letter = 'a'; letter <= 'z' ; letter++)
 putchar(letter);
-if (letter == 'q' || letter == 'e')
+if (letter != 'e' && letter != 'q')
 putchar('\n');
 return (0);
 }
