bd631c7d3ad0740ad601a077608c3269b9a2cf0a
MaFlet <mfletcher2808@hotmail.com>
2024-06-20 02:24:26 +0000

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -5,9 +5,9 @@
 */
 int main(void)
 {
-int num;
-for (num = 0; num < 10 ; num++)
-putchar("%d", num);
-putchar("\n");
+int x;
+for (x = 0; x < 10 ; x++)
+putchar(x + '0');
+putchar(' ,');
 return (0);
 }
