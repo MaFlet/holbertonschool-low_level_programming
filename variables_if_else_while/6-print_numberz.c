169063dab47645840e5f8d4b1e7da6d304386acd
MaFlet <mfletcher2808@hotmail.com>
2024-06-20 02:18:25 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10 ; num++)
+putchar("%d", num);
+putchar("\n");
+return (0);
+}
