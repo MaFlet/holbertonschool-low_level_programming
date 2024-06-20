24c812dad4c82ca9ad6bebdfd2c45c0fffee7abd
MaFlet <mfletcher2808@hotmail.com>
2024-06-20 02:09:02 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int x;
+for (x = 0; x < 10 ; x++)
+if (x < 9)
+printf(", ");
+printf("\n");
+return (0);
+}
