3b90bef1283abf36ba20bee38495fd909639d3ad
MaFlet <mfletcher2808@hotmail.com>
2024-06-22 12:26:50 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include "main.h"
 /**
 *main - Entry site
-*Return: 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
+{
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-letter = 'a';
-while (letter <= 'z')
-{
 _putchar(letter);
-letter++;
-}
 _putchar('\n');
-return (0);
 }
