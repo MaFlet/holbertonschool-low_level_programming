a5e28f9f02ab71c5c091b30c2e0d0374777331a7
MaFlet <mfletcher2808@hotmail.com>
2024-06-23 10:11:37 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - adds 2 vintegers and returns the result
+*@num1: first number to compute
+*@num2: second number to compute
+*Return: integer result
+*/
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
