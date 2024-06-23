cd903d6c00a4e1ea3788bc82f8adefa2b449d2f3
MaFlet <mfletcher2808@hotmail.com>
2024-06-23 00:46:02 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,13 +2,23 @@
 /**
 *print_sign - prints the sign of number
 *@n: check character
-*Return: 1 if n is greater than 0 and -1 if less
+*Return: 1 + if n is greater than 0 and -1 if less and 0 if otherwise
 */
 int print_sign(int n)
 {
-if ((n > '0') || (n == '0') ||  n <= '0')
+if (n > 0)
 {
+_putchar(43);
 return (1);
 }
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
 return (0);
+}
 }
