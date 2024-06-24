8095e4f4eb872acc36822033b04984482930082e
MaFlet <mfletcher2808@hotmail.com>
2024-06-24 10:55:22 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - multiply 2 integers
+*@a: first integer to multiply
+*@b: second integer to multiply
+*Return: integer value
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
