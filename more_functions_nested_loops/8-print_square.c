52ad619fd62cdf269a04bd1413c8f7dca6b0a01e
MaFlet <mfletcher2808@hotmail.com>
2024-06-25 10:54:21 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_square - prints a square
+*@#: use to print square
+*@size: size of square
+*if size is 0 or less print new line
+*Return: 0 (Success)
+*/
+void print_square(int size)
+{
+int i, c;
+if (size <= 1)
+_putchar('\n');
+for (i = 1; i < size; i++)
+{
+for (c = 1; c < size; c++)
+{
+_putchar(' ');
+}
+_putchar('#');
+_putchar('\n');
+}
+}
