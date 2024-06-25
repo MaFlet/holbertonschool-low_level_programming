8289d882f2fe895eb331a3b7028b8b8903a9c745
MaFlet <mfletcher2808@hotmail.com>
2024-06-25 11:58:40 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*print_square - prints a square
+*@#: use to print square
+*@size: size of square
+*if size is 0 or less print new line
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int i, c;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (c = 0; c < (size - 1); c++)
+_putchar('#');
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
