4c61867d03e10faf120d60dfe42bb1b4f5e6582d
MaFlet <mfletcher2808@hotmail.com>
2024-06-25 13:11:19 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,15 +9,20 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0)
-printf("Fizz");
-if (i % 5 == 0)
-printf("Buzz");
+while (i++ < 100)
 if ((i % 3 != 0) && (i % 5 != 0))
 printf("FizzBuzz");
-printf("\n");
+else if ((i % 3) == 0)
+printf("Fizz");
+else if ((i % 5) == 0)
+{
+if (i != 100)
+printf("Buzz");
+else
+printf("Buzz");
 }
+else
+printf("%d", i);
+printf("\n");
 return (0);
 }
