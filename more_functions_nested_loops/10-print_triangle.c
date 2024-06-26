128b4c45637c6add44352aa588969a816d3f6345
root <mfletcher2808@hotmail.com>
2024-06-26 11:41:29 +0000

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*print_triangle - prints a triangle
+*@#: use to print triangle
+*@size: size of square
+*if size is 0 or less print new line
+*Return: 0 (Success)
+*/
+void print_triangle(int size)
+{
+int i, c;
+int space;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (space = size * i; space >= 1; space++)
+{
+_putchar(' ');
+}
+for (c = 1; c <= i; c++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
