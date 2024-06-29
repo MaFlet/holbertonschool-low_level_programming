073ed4d07a9089a6c6d420115e038fb7da1c23d5
MaFlet <mfletcher2808@hotmail.com>
2024-06-29 11:42:45 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,16 @@
 */
 void print_rev(char *s)
 {
-while (*s != '\0')
+int i, c, len;
+i = 0;
+while (s[i] != '\0')
 {
-_putchar(*s--);
+i++;
+}
+len = i;
+for (c = len - 1; c >= 0; c--)
+{
+_putchar(s[c]);
 }
 _putchar('\n');
 }
