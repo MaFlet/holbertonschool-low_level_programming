162b5a379f06b118d5ac09b0147b45e946ff8660
MaFlet <mfletcher2808@hotmail.com>
2024-06-30 01:14:56 +0000

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*puts_half - prints half of string
+*@str: character to be checked
+*Return: print
+*/
+void puts_half(char *str)
+{
+int i;
+int n;
+int count = 0;
+for (i = 0; str[i] != '\0'; i++)
+count++;
+n = (count - 1) / 2;
+for (i = n + 1; str[i] != '\0'; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
