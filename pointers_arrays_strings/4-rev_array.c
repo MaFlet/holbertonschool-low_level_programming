a31fa86fd3b6a4d420905202f038231321f82f1e
MaFlet <mfletcher2808@hotmail.com>
2024-06-30 07:32:48 +0000

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*reverse_array - reverses content array of integers
+*@a: integer
+*@n: number of array elements
+*Return: void
+*/
+void reverse_array(int *a, int n)
+{
+int i;
+int c;
+for (i = 0; i < n--; i++)
+{
+c = a[i];
+a[i] = a[i];
+a[n] = c;
+}
+}
