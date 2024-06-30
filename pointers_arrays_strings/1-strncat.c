651176f7df1ce2846b01af4cd3acd85557bc7e18
MaFlet <mfletcher2808@hotmail.com>
2024-06-30 06:09:42 +0000

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+**_strncat - concatenates 2 strings
+*@dest: string appended
+*@src: character to append
+*@n: character check
+*Return: *dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i, c;
+i = 0;
+c = 0;
+while (dest[i] != '\0')
+i++;
+while (src[c] != '\0' && c < n)
+{
+dest[i] = src[c];
+i++;
+c++;
+}
+dest = '\0';
+return (dest);
+}
