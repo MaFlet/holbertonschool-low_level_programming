961b6eac442ecc9f024c27d2eb996a6a71965e6d
MaFlet <mfletcher2808@hotmail.com>
2024-07-06 23:55:00 +0000

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_puts_recursion - prints a string
+*@s: character to be checked
+*Return: void
+*/
+void _puts_recursion(char *s)
+{
+int i;
+i = 0;
+_putchar(s[i]);
+_putchar('\n');
+}
