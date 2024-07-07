1fc292841841c341b3e9d92d5fa4fdeaf70978f7
MaFlet <mfletcher2808@hotmail.com>
2024-07-07 00:46:00 +0000

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_print_rev_recursion - prints string in reverse
+*@s: string in reverse
+*Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+s--;
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_print_rev_recursion(s - 1);
+}
