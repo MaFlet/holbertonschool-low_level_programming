b930a4ba9394e58fbd91ee03160e712433590f8a
MaFlet <mfletcher2808@hotmail.com>
2024-07-07 08:21:26 +0000

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_pow_recursion - returns value x raised to
+*the power of y
+*@x: integer
+*@y: power value
+*Return: -1 if y is lower than 0
+*/
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
