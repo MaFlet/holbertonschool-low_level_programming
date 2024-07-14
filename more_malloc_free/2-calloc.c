940692c69c2ffa3c19b92bee2ece9211092c2b74
MaFlet <mfletcher2808@hotmail.com>
2024-07-14 02:22:19 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,35 +10,21 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int i = 0, c = 0, j = 0, a = 0;
-char *string;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[i])
-i++;
-while (s2[j])
-j++;
-if (n >= j)
-a = i + j;
-else
-a = i + n;
-string = malloc(sizeof(char) * a + 1);
-if (string == NULL)
+unsigned int i = 0;
+char *array;
+if (nmemb == 0 || size == 0)
+{
 return (NULL);
-j = 0;
-while (c < a)
-{
-if (c <= i)
-string[c] = s1[c];
-if (c >= i)
-{
-string[c] = s2[j];
-j++;
 }
-c++;
+array = malloc(nmemb * size);
+if (array == NULL)
+{
+return (NULL);
 }
-string[c] = '\0';
-return (string);
+while (i < (nmemb * size))
+{
+array[i] = 0;
+i = i + 1;
+}
+return (array);
 }
