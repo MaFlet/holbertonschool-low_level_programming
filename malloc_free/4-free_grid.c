837605dd70749c338b3b818eff310a7d29cb5265
MaFlet <mfletcher2808@hotmail.com>
2024-07-14 10:18:08 +0000

    4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*free_grid - frees 2 dimensional grid created by
+*alloc_grid function
+*@grid: pointer to alloc_grid function
+*@height: height array
+*Return: NULL on failure and if width and height is 0 or negative
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; ++i)
+{
+free(grif[i]);
+}
+free(grid);
+}
