f0d504d250c6a48dbffcc4eec2cbf0b7cff74f15
MaFlet <mfletcher2808@hotmail.com>
2024-07-17 12:05:28 +0000

    3-calc.h and 3-op_functions. are added

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*op_add - addition operator
+*@a: first integer
+*@b: second integer
+*Return: result
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+*op_sub - subtraction operator
+*@a: first integer
+*@b: second integer
+*Return: result
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+*op_mul - multiplication operator
+*@a: first integer
+*@b: second integer
+*Return: result
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+*op_div - division operator
+*@a: first integer
+*@b: second integer
+*Return: result
+*/
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+/**
+*op_mod - modulus operator
+*@a: first integer
+*@b: second integer
+*Return: result
+*/
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+result (a % b);
+}
