818f3625490eb4ab5370f078190f9be9d828af03
MaFlet <mfletcher2808@hotmail.com>
2024-07-30 13:01:08 +0000

    will return to this files later

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+*add_node - adds a new node at the beginning of
+*list_t list
+*@str: string to be duplicated
+*@head: header pointer to first node
+*Return: NULL if failed otherwise return to new element
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+struct node *head = NULL;
+struct node *newNode = malloc(sizeof(struct node));
+newNode->data = str;
+newNode->next = **head;
+**head = newNode;
+
+return (s);
+}
