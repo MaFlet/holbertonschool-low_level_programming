4d4ff9202ed528955195cf273915c433dda85353
MaFlet <mfletcher2808@hotmail.com>
2024-08-03 00:45:01 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,17 +4,6 @@
 #include <stddef.h>
 #include <string.h>
 /**
-*strdup - duplicate a string
-*@s: string to be duplicated
-*Return: duplicated string
-*/
-char *strdup(const char *s)
-{
-char *str = strdup(s);
-printf("%s", str);
-return (str);
-}
-/**
 *add_node - adds a new node at the beginning of
 *list_t list
 *@str: string to be duplicated
@@ -23,16 +12,16 @@ return (str);
 */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
-unsigned int len = 0;
+list_t *temp;
+int len = 0;
+temp = malloc(sizeof(list_t));
+if (temp == NULL)
+return (NULL);
 while (str[len])
 len++;
-new = malloc(sizeof(list_t));
-if (!new)
-return (NULL);
-new->str = strdup(str);
-new->len = len;
-new->next = (*head);
-(*head) = new;
-return (*head);
+temp->str = strdup(str);
+temp->len = len;
+temp->next = *head;
+*head = temp;
+return (temp);
 }
