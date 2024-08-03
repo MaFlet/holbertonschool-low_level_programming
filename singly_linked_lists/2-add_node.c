381d535fc936b874794409fad9d264ece8e4290b
MaFlet <mfletcher2808@hotmail.com>
2024-08-03 00:34:07 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -23,24 +23,16 @@ return (str);
 */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
+list_t *new;
 unsigned int len = 0;
 while (str[len])
 len++;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-free(new_node);
+new = malloc(sizeof(list_t));
+if (!new)
 return (NULL);
-{
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-new_node->len = len;
-new_node->next = *head;
-*head = new_node;
+new->str = strdup(str);
+new->len = len;
+new->next = (*head);
+(*head) = new;
 return (*head);
-}
-}
 }
