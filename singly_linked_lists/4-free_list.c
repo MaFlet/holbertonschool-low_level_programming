1137ebb35ed466d5aa06c6830968b26e337fe200
MaFlet <mfletcher2808@hotmail.com>
2024-08-03 01:41:42 +0000

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+*free_list - frees a list_t list
+*@head: pointer to free list_t list
+*Return: void
+*/
+void free_list(list_t *head)
+{
+list_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
