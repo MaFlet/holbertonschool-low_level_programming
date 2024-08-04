f47055a419c342a941f8b5b979484f5b27b8dd06
MaFlet <mfletcher2808@hotmail.com>
2024-08-04 00:25:38 +0000

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,27 +15,20 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 dlistint_t *new_node;
 dlistint_t *temp;
 new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 new_node->n = n;
 new_node->next = NULL;
-if (*head == NULL)
+temp = *head;
+if (temp != NULL)
 {
-*head = new_node;
-new_node->prev = NULL;
-return (NULL);
-}
 while (temp->next != NULL)
 temp = temp->next;
-temp->next = new_node;
-new_node->prev = temp;
-return (temp);
-{
-new_node = malloc(sizeof(dlistint_t));
-new_node->n = n;
-new_node->next = *head;
-new_node->prev = NULL;
-if (*head != NULL)
-(*head)->prev = new_node;
-*head = new_node;
-return (new_node);
 }
+else
+{
+*head = new_node;
+}
+new_node->prev = temp;
+return (new_node);
 }
