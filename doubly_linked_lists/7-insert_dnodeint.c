3cbfa9a8111fa8a2fcc6ee5916629d28229e29d5
MaFlet <mfletcher2808@hotmail.com>
2024-08-04 02:17:59 +0000

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,36 +13,35 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *temp;
-dlistint_t *new_node;
-unsigned int count;
-temp = *h;
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->prev = NULL;
-new_node->next = NULL;
+unsigned int count = 0;
+dlistint_t *new_node = malloc(sizeof(dlistint_t)), *temp = *h;
+if (new_node)
+{
 new_node->n = n;
 if (idx == 0)
 {
+if (*h)
+{
+new_node->next = *h;
+*h->prev = new_node;
+}
 *h = new_node;
 return (new_node);
 }
-else
-{
-count = 0;
-while (temp != NULL && count != idx - 1)
+while (count < idx - 1 && temp)
 {
 temp = temp->next;
 count++;
 }
-if (count != idx - 1)
+if (temp)
 {
-return (NULL);
-}
-temp->next = new_node;
-new_node->next = temp;
+if (temp->next)
+temp->next->prev = new_node;
+new_node->next = temp->next;
 new_node->prev = temp;
+temp->next = new_node;
 return (new_node);
 }
+}
+return (NULL);
 }
